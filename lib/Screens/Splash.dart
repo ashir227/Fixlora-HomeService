fe9e5fc188235ddae0e97779037e01a1540212b7
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-06-26 11:53:24 +0500

    asset image

--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -1,21 +1,39 @@
 import 'package:fixlora/theme/colors.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 class SplashScr extends StatelessWidget {
   const SplashScr({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: AppColors.primary,
       body: Center(
-        child: Text(
-          "FIXLORA",
-          style: TextStyle(
-            color: AppColors.primarySoft,
-            fontSize: 35,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.primaryLight,
+                borderRadius: BorderRadius.circular(33),
+              ),
+              // child: Text("data"),
+              width: w * 0.35,
+              height: h * 0.18,
+            ),
+            SizedBox(height: h * 0.03),
+            Text(
+              "FIXLORA SERICEHUB",
+              style: TextStyle(
+                color: AppColors.primarySoft,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
