1c9090e11f10df21eee341446caa352ccabd0cf0
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-06-29 21:48:48 +0500

    Add login scr

---
+++ New folder/lib/Screens/Splash.dart
@@ -0,0 +1,66 @@
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
+import 'package:fixlora/Screens/login.dart';
+import 'package:fixlora/theme/colors.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+
+class SplashScr extends StatefulWidget {
+  const SplashScr({super.key});
+
+  @override
+  State<SplashScr> createState() => _SplashScrState();
+}
+
+class _SplashScrState extends State<SplashScr> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {});
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => loginscr()),
+      );
+    });
+  }
+
+  Widget build(BuildContext context) {
+    final h = MediaQuery.of(context).size.height;
+    final w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.primaryLight,
+                borderRadius: BorderRadius.circular(33),
+              ),
+
+              // child: Text("data"),
+              width: w * 0.35,
+              height: h * 0.18,
+              child: Image.asset(
+                "assets/images/icon_only.png",
+                height: h * 0.01,
+                width: w * 0.01,
+              ),
+            ),
+            SizedBox(height: h * 0.03),
+            Text(
+              "FIXLORA SERICEHUB",
+              style: TextStyle(
+                color: AppColors.primarySoft,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ New folder/lib/Screens/login.dart
@@ -0,0 +1,11 @@
+import 'package:fixlora/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class loginscr extends StatelessWidget {
+  const loginscr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: AppColors.success);
+  }
+}
