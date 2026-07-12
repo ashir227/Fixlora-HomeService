b5829b12f113110a5d1b35537056364fed7b26a2
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-07-12 19:33:10 +0500

    Login

--- New folder/lib/Screens/loginscr.dart
+++ New folder/lib/Screens/loginscr.dart
@@ -1,4 +1,5 @@
 import 'package:fixlora/theme/colors.dart';
+import 'package:fixlora/widgets/cstmtxt.dart';
 import 'package:flutter/material.dart';
 
 class Loginscr extends StatelessWidget {
@@ -11,20 +12,35 @@ class Loginscr extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColors.surface,
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: w * 0.05),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: h * 0.1),
-            Image.asset(
-              "assets/images/smal_log.png",
-              height: h * 0.08,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: h * 0.06),
-            Text("Welcome back"),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: h * 0.1),
+              Image.asset(
+                "assets/images/smal_log.png",
+                height: h * 0.08,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: h * 0.06),
+              reusetext(
+                context: context,
+                txt: "Welcome Back",
+                clr: AppColors.textPrimary,
+                Size: w * 0.08,
+                FontWeight: FontWeight.bold,
+              ),
+              reusetext(
+                context: context,
+                txt: "Sign in to continue Fixlora",
+                clr: AppColors.textDisabled,
+                Size: w * 0.04,
+                FontWeight: FontWeight.w500,
+              ),
+            ],
+          ),
         ),
       ),
     );
