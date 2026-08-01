ae532e66a6470c81e96c6d520295ccb20125beb1
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-08-01 12:08:38 +0500

    Add fire base

--- New folder/lib/Screens/Signup.dart
+++ New folder/lib/Screens/Signup.dart
@@ -1,4 +1,5 @@
 import 'package:fixlora/theme/colors.dart';
+import 'package:fixlora/widgets/cstm_field.dart';
 import 'package:fixlora/widgets/cstmtxt.dart';
 import 'package:flutter/material.dart';
 
@@ -7,21 +8,36 @@ class Signup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController namecntrl = TextEditingController();
+
     final h = MediaQuery.of(context).size.height;
     final w = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: AppColors.surface,
+
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Container(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               reusetext(
                 context: context,
                 txt: "Create account",
+
                 clr: AppColors.textPrimary,
                 Size: w * 0.08,
                 FontWeight: FontWeight.bold,
               ),
+              reusetext(
+                context: context,
+                txt: "join our most trusted platform",
+                clr: AppColors.textHint,
+                Size: w * 0.04,
+                FontWeight: FontWeight.w400,
+              ),
+
+              CstmFld(validator: (value) {}, controller: namecntrl),
             ],
           ),
         ),
